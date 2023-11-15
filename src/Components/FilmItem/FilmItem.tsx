6e26e4139f2bfa6620ece2204f058d37aa18b3b6
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-11-15 20:36:52 +0600

    added FilmItem deletion & changing name

--- src/Components/FilmItem/FilmItem.tsx
+++ src/Components/FilmItem/FilmItem.tsx
@@ -3,16 +3,19 @@ import {Film} from '../../types';
 
 export interface FilmItemProps {
   film: Film;
+  deleteFilmItem: (id: number) => void;
+  changeFilmName: (id: number, e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FilmItem: React.FC<FilmItemProps> = React.memo(function FilmItem({film}) {
+
+const FilmItem: React.FC<FilmItemProps> = React.memo(function FilmItemTest({film, deleteFilmItem, changeFilmName}) {
   return (
     <div>
-      <input type="text" value={film.name}/>
-      <button type="button">X</button>
+      <input type="text" value={film.name} onChange={e => changeFilmName(film.id, e)}/>
+      <button type="button" onClick={() => deleteFilmItem(film.id)}>X</button>
     </div>
   );
 }, (prevProps, nextProps) => {
-  return prevProps.film.name === nextProps.film.name;
+  return prevProps.film.name !== nextProps.film.name && prevProps.film.id !== nextProps.film.id;
 });
 export default FilmItem;
--- src/Container/FilmsTracker/FilmsTracker.tsx
+++ src/Container/FilmsTracker/FilmsTracker.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import FilmItem from '../../Components/FilmItem/FilmItem';
 
 const FilmsTracker = () => {
@@ -9,10 +9,30 @@ const FilmsTracker = () => {
     {name: 'Bad boys', id: 3},
   ]);
 
+  useEffect(() => {
+    console.log(films)
+  }, [films]);
+
+  const deleteFilmItem = (id: number) => {
+    setFilms(films.filter(filmObject=> filmObject.id !== id));
+  };
+  
+  const changeFilmName = (id: number, e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilms(films.map(filmObject => {
+      if (id === filmObject.id) {
+        return {
+          ...filmObject,
+          name: e.target.value
+        };
+      }
+      return filmObject;
+    }));
+  };
+
   return (
     <>
       {films.map(anyFilm =>(
-        <FilmItem key={anyFilm.id} film={anyFilm}/>
+        <FilmItem key={anyFilm.id} film={anyFilm} deleteFilmItem={deleteFilmItem} changeFilmName={changeFilmName}/>
       ))}
     </>
   );
